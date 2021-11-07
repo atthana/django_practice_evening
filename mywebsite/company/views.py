a452f11dd0f52d011024e935398feeb6f00d9a88
atthana-p <atthana.p@codium.co>
2021-11-08 00:50:04 +0700

    เพิ่มเงื่อนไขใน views.py เมื่อ save ลง db ก็จะมีข้อความบอก  user ด้วย

--- mywebsite/company/views.py
+++ mywebsite/company/views.py
@@ -2,7 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import Product
+from .models import ContactList, Product
 
 def Home(request): # ต้องใส่ request เข้าไปด้วยนะ เพราะ render ต้องการ request เป็นส่วนประกอบ
 
@@ -15,9 +15,12 @@ def AboutUs(request):
     return render(request, 'company/aboutus.html')
 
 def ContactUs(request):
+
+    context = {} # สิ่งที่จะแนบไป คือเวลาเข้ามาในหน้านี้ปกติ จะทำบรรทัดนี้
+
     if request.method == 'POST':
-        data = request.POST.copy()
-        title = data.get('title')
+        data = request.POST.copy()  # data จะเป็น dictinary นะ
+        title = data.get('title')  # ชื่อพวกนี้ควรเหมือนกับ id ใน html นะ
         email = data.get('email')
         detail = data.get('detail')
         print(data)
@@ -25,5 +28,15 @@ def ContactUs(request):
         print(title)
         print(email)
         print(detail)
-        print('------------')
-    return render(request, 'company/contact.html')
+        print('------------')  # ปกติควร print ก่อนนะ พอได้ข้อมูลแล้วค่อยทำการ save
+
+        # ContactUs(title=title, email=email, detail=detail).save()  # ทำแบบนี้ก็ save ได้เหมือนกันนะ ลด code ให้เหลือบรรทัดเดียวเลย แต่จะไม่ยืดหยุ่นเหมือนแบบหลายบรรทัดข้างล่างที่มันใส่เงื่อนไขแต่ละ field ได้
+        
+        newrecord = ContactList()
+        newrecord.title = title  # เพราะเราสร้าง model แล้ว เลยใช้ .title ได้เลย เหมือนกับ Flutter ที่สร้าง model นั่นแหละ
+        newrecord.email = email  # พวกนี้คือการใส่ค่าเข้าไปใน model น่ั่นแหละ
+        newrecord.detail = detail
+        newrecord.save()  # พอใช้ .save() ก็จะ save ข้อมูลเข้า db นะ
+        context['message'] = 'ตอนนี้คิวได้รับข้อความแล้ว เด่วขออ่านก่อนนะแล้วจะตอบกลับภายใน 5 ปี'
+
+    return render(request, 'company/contact.html', context=context)
